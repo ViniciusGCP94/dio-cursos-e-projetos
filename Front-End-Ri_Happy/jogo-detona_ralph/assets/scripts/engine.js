07a6aa056b2ed29f37c5b35e375b1f981387368d
ViniciusGCP94 <vinniegcp@gmail.com>
2024-10-27 22:33:53 -0300

    adicionando clicks e pontuação a cada click

--- Front-End-Ri_Happy/jogo-detona_ralph/assets/scripts/engine.js
+++ Front-End-Ri_Happy/jogo-detona_ralph/assets/scripts/engine.js
@@ -9,6 +9,8 @@ const state ={
     values: {
         timerId: null,
         gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     },
 };
 
@@ -19,7 +21,8 @@ function randomSquare (){
 
     let randomNumber = Math.floor(Math.random() * 9)
     let randomSquare = state.view.squares[randomNumber]
-    randomSquare.classList.add("enemy")
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy(){
@@ -27,11 +30,20 @@ function moveEnemy(){
 }
 
 function addListenerHitbox(){
-    state.view.squares.forEach((square) =>{});
+    state.view.squares.forEach((square) =>{
+        square.addEventListener("mousedown", () => {
+            if (square.id === state.values.hitPosition){
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
+    });
 }
 
 function init () {
-    moveEnemy()
+    moveEnemy();
+    addListenerHitbox()
 }
 
 init();
