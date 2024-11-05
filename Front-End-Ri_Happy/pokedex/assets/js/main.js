7e0e711e417fc6a9711ab5d29d36d1e425d95e7b
ViniciusGCP94 <vinniegcp@gmail.com>
2024-11-04 23:59:53 -0300

    utilizando arrow function para ter um codigo  menos verboso

--- Front-End-Ri_Happy/pokedex/assets/js/main.js
+++ Front-End-Ri_Happy/pokedex/assets/js/main.js
@@ -3,23 +3,6 @@ const limit = 10;
 const url =  `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
 fetch(url)
-    .then(function(response){ // promise como resposta
-        return response.json() // conversão para JSON
-    })
-    .then(function(jsonBody){ //Body convertido
-        console.log(jsonBody) // imprima o Body
-    })
-    .catch(function(error){
-    console.error(error)
-    })
-    .finally(function(){
-        console.log('Requisição concluída!')
-    })
-
-try{
-
-} catch (error) {
-
-} finally {
-
-}
+    .then((response) => response.json())    
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.error(error))
