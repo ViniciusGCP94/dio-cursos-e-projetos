0aad2515f2c5f68e9c8b4f8a3fadf751b3a0d2a0
ViniciusGCP94 <vinniegcp@gmail.com>
2024-11-07 00:59:11 -0300

    multiplas requisições em paralelo

--- Front-End-Ri_Happy/pokedex/assets/js/pokeAPI.js
+++ Front-End-Ri_Happy/pokedex/assets/js/pokeAPI.js
@@ -1,9 +1,25 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit =10 ) => {
+pokeApi.getPokemonsDetails = (pokemon) => {
+    return fetch(pokemon.url)
+            .then((response) => response.json())
+}
+
+pokeApi.getPokemons = (offset = 0, limit = 5 ) => {
     const url =  `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results) 
-        .catch((error) => console.error(error))
+        .then((jsonBody) => jsonBody.results)
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonsDetails))
+        .then((detailRequests) => Promise.all(detailRequests))
+        .then((pokemonsDetails) => pokemonsDetails)
 }
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4'),
+    fetch('https://pokeapi.co/api/v2/pokemon/5')
+]).then((results) => {
+    console.log(results)
+})
