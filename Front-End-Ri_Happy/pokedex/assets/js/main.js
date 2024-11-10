41448103f7edec48aaebe502fb34d3cf6fe7ecb8
ViniciusGCP94 <vinniegcp@gmail.com>
2024-11-10 12:50:39 -0300

    adicionando background a cada tipo de pokemon, centralizando o 1º tipo e letramaiuscula no nome

--- Front-End-Ri_Happy/pokedex/assets/js/main.js
+++ Front-End-Ri_Happy/pokedex/assets/js/main.js
@@ -1,12 +1,12 @@
 
 function convertPokemonLi (pokemon) {
     return `
-        <li class="pokemon">
+        <li class="pokemon ${pokemon.typePrincipal}">
             <span class="number">${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
             <div class="detail">
                 <ol class="types">
-                    ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
                 </ol>
                 <img src="${pokemon.picture}" alt="${pokemon.name}">
             </div>       
@@ -17,5 +17,5 @@ function convertPokemonLi (pokemon) {
 const pokemonList = document.getElementById('pokemonList');
 
 pokeApi.getPokemons().then((pokemons = []) => {
-    pokemonList.innerHTML += pokemons.map(convertPokemonLi).join('')
+    pokemonList.innerHTML = pokemons.map(convertPokemonLi).join('')
 })
--- Front-End-Ri_Happy/pokedex/assets/js/pokeAPI.js
+++ Front-End-Ri_Happy/pokedex/assets/js/pokeAPI.js
@@ -24,7 +24,7 @@ pokeApi.getPokemonsDetails = (pokemon) => {
         .then(convertPokeApiDetailPokemon)
 }
 
-pokeApi.getPokemons = (offset = 0, limit = 5 ) => {
+pokeApi.getPokemons = (offset = 0, limit = 25 ) => {
     const url =  `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
     return fetch(url)
         .then((response) => response.json())
