b3b2d044bdaf78d0947c78cf7919a1ecb2a2403a
ViniciusGCP94 <vinniegcp@gmail.com>
2024-11-12 20:38:49 -0300

    criação dos states da Engine e enumeração as cartas

---
+++ Front-End-Ri_Happy/yu-gi-oh/src/scripts/engine.js
@@ -0,0 +1,59 @@
+const state = {
+    score:{
+        playerScore:0,
+        computerScore:0,
+        scoreBox: document.getElementById("score-points")
+    },
+
+    cardSprites:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type")
+    },
+
+    fieldCards:{
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+
+    actions:{
+        resetButton:document.getElementById("next-duel")
+    },
+}
+
+const pathImages = ".src/assets/icons/"
+
+const cardData = [
+    {
+        id:0,
+        name:"Blue Eyes White Dragon",
+        type:"paper",
+        image: `${pathImages}dragon.png`,
+        winsAgainst: [2], // Ganha do Exodia
+        losesTo: [1] // Perde para Dark Magician
+    },
+
+    {
+        id: 1,
+        name: 'Dark Magician',
+        type: 'rock',
+        imagePath: `${baseImagePath}/magician.png`,
+        winsAgainst: [0], // Ganha do Blue-Eyes White Dragon
+        losesTo: [2] // Perde para Exodia
+    },
+    
+    {
+        id: 2,
+        name: 'Exodia',
+        type: 'scissors',
+        imagePath: `${baseImagePath}/exodia.png`,
+        winsAgainst: [1], // Ganha do Dark Magician
+        losesTo: [0] // Perde para Blue-Eyes White Dragon
+    }
+]
+
+function init( ) {
+
+}
+
+init();
