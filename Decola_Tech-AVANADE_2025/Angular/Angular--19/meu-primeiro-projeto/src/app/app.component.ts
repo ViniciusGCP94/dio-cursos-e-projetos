a00898109c52c382c350170b9116870c8f567c9f
ViniciusGCP94 <vinniegcp@gmail.com>
2025-03-01 14:05:17 -0300

    adicionando propriedades dinamicamente  aos botões

--- Decola_Tech-AVANADE_2025/Angular/Angular--19/meu-primeiro-projeto/src/app/app.component.ts
+++ Decola_Tech-AVANADE_2025/Angular/Angular--19/meu-primeiro-projeto/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { buttonComponent } from "./components/button/button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, buttonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
